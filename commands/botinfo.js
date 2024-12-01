c92faac84edb9986551715e47f5e1defe084fbf6
Brant <itsbrantttv@gmail.com>
2024-12-01 02:16:38 -0500

    Added version, modified thumbnail to be a high-res image rather than lowres avatar.

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { version } = require('../package.json'); // Import version from package.json
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,14 +12,15 @@ module.exports = {
         const uptimeString = new Date(uptime * 1000).toISOString().substr(11, 8);
 
         const embed = new EmbedBuilder()
-            .setColor(0x3498DB)
+            .setColor(0x7700ff)
             .setTitle(`Hi, I'm ${bot.username}!`)
-            .setThumbnail(bot.displayAvatarURL())
+            .setThumbnail('https://i.imgur.com/vsLDxkK.png')
             .setDescription(
                 "I'm here to help the Voids Within team with some moderation as well as the community challenges!"
             )
             .addFields(
                 { name: 'Developer', value: `<@862537604138401822>`, inline: true },
+                { name: 'Version', value: `v${version}`, inline: true }, // Added version
                 { name: 'Uptime', value: `${uptimeString}`, inline: true },
                 { name: 'Users in Server', value: `${totalUsers}`, inline: true },
                 { name: 'Library', value: 'Discord.js v14', inline: true }
